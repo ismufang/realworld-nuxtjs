37fc0dbccc60fbb30ef6247b0099a4ec780ceceb
ismufang <446379625@qq.com>
2020-09-05 02:40:43 +0800

    0.1.5

--- store/index.js
+++ store/index.js
@@ -3,7 +3,7 @@ const cookieparser = process.server ? require('cookieparser') : undefined
 export const state = () => {
   return {
     user: null,
-    version: '0.1.4'
+    version: '0.1.5'
   }
 }
 
